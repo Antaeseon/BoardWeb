23d2f97a4485a09f8abbb6e827832565745511bc
Antaeseon <ants412@naver.com>
2021-02-11 17:57:22 +0900

    chap 5 clear

--- src/main/java/polymorphism/SonySpeaker.java
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -1,5 +1,7 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
 public class SonySpeaker implements Speaker{
 	public SonySpeaker() {
 		System.out.println("===> SonySpeake 객체 생성");
