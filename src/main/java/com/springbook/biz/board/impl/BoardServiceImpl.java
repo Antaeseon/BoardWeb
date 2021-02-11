85fa15b112c05c48f212f4f078c1cf15c6378991
Antaeseon <ants412@naver.com>
2021-02-12 01:57:35 +0900

    Day 2 chap 1 Clear!

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -7,29 +7,31 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.Log4jAdvice;
+import com.springbook.biz.common.LogAdvice;
 
 @Service("boardService")
-public class BoardServiceImpl implements BoardService{
+public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
 
 	public void insertBoard(BoardVO vo) {
 		boardDAO.insertBoard(vo);
 	}
-	
+
 	public void updateBoard(BoardVO vo) {
 		boardDAO.updateBoard(vo);
 	}
-	
+
 	public void deleteBoard(BoardVO vo) {
 		boardDAO.deleteBoard(vo);
 	}
-	
+
 	public BoardVO getBoard(BoardVO vo) {
 		return boardDAO.getBoard(vo);
 	}
-	
-	public List<BoardVO> getBoardList(BoardVO vo){
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
 		return boardDAO.getBoardList(vo);
 	}
 }
